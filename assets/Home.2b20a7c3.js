aafca7399199989456f81435e6146682d3e97bc8
Victor Ubere <victorubere@gmail.com>
2022-10-25 04:41:06 +0100

    Updates

---
+++ assets/Home.2b20a7c3.js
@@ -0,0 +1,1 @@
+import{a as e,b as s}from"./index.8d02403f.js";function n(){return e("main",{children:e(s,{to:"/users",children:e("button",{className:"buttonToUser",children:"see users"})})})}export{n as default};
